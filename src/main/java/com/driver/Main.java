499ddab49a026c9e6d47c932efc813b521e2ebd4
Shubham <shubhamdesai2405@gmail.com>
2022-10-09 20:45:59 +0530

    changes

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -2,7 +2,9 @@ package com.driver;
 
 public class Main {
     public static void main(String[] args) {
-        A obj = new B();
+        A obj = new A();
         System.out.println(obj.meth());
+        A obj1 = new B();
+        System.out.println(obj1.meth());
     }
 }
