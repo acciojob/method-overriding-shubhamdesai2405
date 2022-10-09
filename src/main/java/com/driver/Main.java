851093336aabd9407e97cc9cfaa63fb7eb9ad6f2
Shubham <shubhamdesai2405@gmail.com>
2022-10-09 20:50:37 +0530

    ohh

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -2,9 +2,9 @@ package com.driver;
 
 public class Main {
     public static void main(String[] args) {
-        A obj = new A();
+        A obj = new B();
         System.out.println(obj.meth());
-        A obj1 = new B();
-        System.out.println(obj1.meth());
+
+        System.out.println(obj.meth());
     }
 }
